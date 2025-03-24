a5243e977e933342df74b84651c68dd756a03629
AkotosiJUSTIN <127915260+AkotosiJUSTIN@users.noreply.github.com>
2025-03-24 21:18:08 +0800

    ne column notif table

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -137,6 +137,7 @@ CREATE TABLE Notifications (
 ALTER TABLE Notifications
 ADD COLUMN trigger_user_id INT DEFAULT NULL,
 ADD FOREIGN KEY (trigger_user_id) REFERENCES Users(id) ON DELETE SET NULL;
+ADD COLUMN is_admin BOOLEAN DEFAULT FALSE;
 
 -- Admins Table
 CREATE TABLE Admins (
